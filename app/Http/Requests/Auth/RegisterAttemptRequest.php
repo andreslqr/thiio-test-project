3f6e0cf0c557fafc3cd1b9ee98c97eb6ebcce32f
andreslqr <me@andreslopez.com.mx>
2024-02-29 16:31:10 -0600

    fix: added rule for avoid duplicates user emails

--- app/Http/Requests/Auth/RegisterAttemptRequest.php
+++ app/Http/Requests/Auth/RegisterAttemptRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules\Password;
 
@@ -31,6 +32,7 @@ class RegisterAttemptRequest extends FormRequest
             'email' => [
                 'required',
                 'string',
+                'unique:' . app(User::class)->getTable() . ',email',
                 'email',
                 'max:255'
             ],
