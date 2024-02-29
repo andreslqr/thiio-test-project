4a72592d027257caa7c486168e2e29d0997c5f91
andreslqr <me@andreslopez.com.mx>
2024-02-29 16:52:53 -0600

    added logic

---
+++ app/Http/Requests/V1/User/CreateUserRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Requests\V1\User;
+
+use App\Http\Requests\Auth\RegisterAttemptRequest;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUserRequest extends RegisterAttemptRequest
+{
+    //
+}
