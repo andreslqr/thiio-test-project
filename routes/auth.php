c08c4e4a0280bccb1050cc5bd9a37a9cf50db4d9
andreslqr <me@andreslopez.com.mx>
2024-02-28 19:54:38 -0600

    feat: refresh token

--- routes/auth.php
+++ routes/auth.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RefreshLoginController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('login', LoginController::class)->withoutMiddleware('auth:api')->name('login');
+
+Route::post('refresh', RefreshLoginController::class)->name('refresh');
